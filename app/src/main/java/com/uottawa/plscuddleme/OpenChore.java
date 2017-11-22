7889ab257ecf19ede3abbb2988af24bde3ac25dd
Simon2233 <simon.fu.1997@gmail.com>
2017-11-22 16:29:39 -0500

    mm

--- app/src/main/java/com/uottawa/plscuddleme/OpenChore.java
+++ app/src/main/java/com/uottawa/plscuddleme/OpenChore.java
@@ -9,7 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +21,28 @@ import java.util.List;
  */
 
 public class OpenChore extends Fragment {
-
+    RelativeLayout tv;
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("OpenChore");
+
+        tv = (RelativeLayout)getView().findViewById(R.id.filter_container);
+
+        Button filterButton = (Button)getView().findViewById(R.id.filterChore);
+        filterButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                setLayoutVisible();
+            }
+        });
+
+        Button closeFilter = (Button)getView().findViewById(R.id.hideFilter);
+        closeFilter.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                setLayoutInvisible();
+            }
+        });
 
         String[] choreList = {"Walk Dog", "Do the Dishes", "Clean Room", "Make Bed", "Take Trash Out"};
         ListView listView = (ListView)getView().findViewById(R.id.list_view);
@@ -38,5 +56,16 @@ public class OpenChore extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.nav_open_chores, container, false);
+    }
+
+    public void setLayoutInvisible() {
+        if (tv.getVisibility() == View.VISIBLE) {
+            tv.setVisibility(View.GONE);
+        }
+    }
+    public void setLayoutVisible() {
+        if (tv.getVisibility() == View.GONE) {
+            tv.setVisibility(View.VISIBLE);
+        }
     }
 }
